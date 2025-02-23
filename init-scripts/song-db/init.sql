ae2a6f6477f8d6e0d2ab45e2bb1d87241fdf24c3
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2025-02-23 20:37:39 +0500

    Implement containerization, fixed song-service url

--- init-scripts/song-db/init.sql
+++ init-scripts/song-db/init.sql
@@ -1,10 +1,11 @@
 DROP TABLE IF EXISTS songs;
 
 CREATE TABLE songs (
-    id BIGINT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    artist VARCHAR(100) NOT NULL,
-    album VARCHAR(100) NOT NULL,
-    duration TEXT NOT NULL,
-    year TEXT NOT NULL
+    id       BIGINT       NOT NULL,
+    album    VARCHAR(100) NOT NULL,
+    artist   VARCHAR(100) NOT NULL,
+    duration VARCHAR(255) NOT NULL,
+    name     VARCHAR(100) NOT NULL,
+    year     VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
--- resource-service/src/main/java/com/musicapp/resource/entity/Resource.java
+++ resource-service/src/main/java/com/musicapp/resource/entity/Resource.java
@@ -18,6 +18,5 @@ public class Resource {
     private Long id;
 
     @Lob
-    @Column(name = "data", columnDefinition = "BYTEA")
     private byte[] data;
 }
